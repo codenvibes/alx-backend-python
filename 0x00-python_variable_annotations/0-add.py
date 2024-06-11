8e23b088a6324ef053e948086207d63978d76fff
codenvibes <terrymk1999@gmail.com>
2024-06-11 13:29:08 +0300

    Feat: Create simple addition type-annotation function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -8,12 +8,5 @@ This module provides a simple addition function for floats.
 def add(a: float, b: float) -> float:
     """
     Adds two floating point numbers.
-
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of a and b.
     """
     return a + b
