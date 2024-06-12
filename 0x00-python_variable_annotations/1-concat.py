cd1455c3399f2aab176a541e3d9651bd1e9cf928
codenvibes <terrymk1999@gmail.com>
2024-06-12 18:25:37 +0300

    Feat: Create concat function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+AUTH: bugsnvibes
+This module provides a simple string concatenation function.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+    
+    Parameters:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+    
+    Returns:
+    str: The concatenated result of str1 and str2.
+    """
+    return str1 + str2
