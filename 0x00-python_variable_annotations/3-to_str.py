09212c89885186da616eec7c962d8b317ecd4078
codenvibes <terrymk1999@gmail.com>
2024-06-16 11:58:25 +0300

    Feat: Create a function that stringifies a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to convert a floating-point number to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to its string representation.
+
+    Args:
+        n (float): The number to be converted to a string.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
