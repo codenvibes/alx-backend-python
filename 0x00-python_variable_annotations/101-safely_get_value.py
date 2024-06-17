dd16392115add7e1d2cec68136f4d6aa5456ba3a
codenvibes <terrymk1999@gmail.com>
2024-06-17 15:59:43 +0300

    Feat: Given the parameters and the return values, add type annotations to the specified function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,28 +3,24 @@
 This module provides utility functions for safely accessing values from
 dictionaries.
 """
-from typing import Mapping, Any, Union, TypeVar, Optional
+from typing import TypeVar, Mapping, Union, Any
 
-T = TypeVar('T')
+T = TypeVar('T', None, Any)
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Optional[T] =
-                     None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
     """
-    Safely retrieves the value associated with the given key from the
-    dictionary.
+    Safely retrieves a value from a dictionary-like object (Mapping).
 
     Args:
-        dct (Mapping): The dictionary or mapping-like object to retrieve the
-        value from.
-        key (Any): The key whose associated value is to be retrieved.
-        default (Optional[T], optional): The default value to return if the
-                                         key is not found in the dictionary.
-                                         Defaults to None.
+    - dct (Mapping): The dictionary or mapping object from which to retrieve the value.
+    - key (Any): The key whose associated value is to be retrieved from the dictionary.
+    - default (Union[Any, None]): The default value to return if the key is not found in the dictionary.
 
     Returns:
-        Union[Any, T]: The value associated with the key if found; otherwise,
-        the default value.
+    - Union[Any, Any]: The value associated with the given key if found in the dictionary, 
+                       otherwise returns the provided default value.
     """
     if key in dct:
         return dct[key]
