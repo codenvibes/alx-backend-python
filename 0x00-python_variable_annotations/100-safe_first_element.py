02f0d206ca67d9e26fd01212bfb760d1cccce269
codenvibes <terrymk1999@gmail.com>
2024-06-17 15:26:13 +0300

    Feat: Augment the given code with the correct duck-typed annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 """
-
+This script contains a function to safely retrieve the first element of a
+sequence. If the sequence is empty, the function returns None.
 """
 from typing import Any, Union, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    
+    Returns the first element of the sequence if it exists, otherwise returns
+    None.
+
+    Parameters:
+    lst (Sequence[Any]): The sequence from which to retrieve the first
+    element.
+
+    Returns:
+    Union[Any, None]: The first element of the sequence if it exists,
+    otherwise None.
     """
     if lst:
         return lst[0]
