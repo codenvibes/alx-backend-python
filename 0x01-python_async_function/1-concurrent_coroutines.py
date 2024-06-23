88566ef4d969463e4b36a77900cdcc0b0630fa5d
codenvibes <terrymk1999@gmail.com>
2024-06-23 14:09:43 +0300

    Feat: Create wait_n function to run multiple async tasks and return sorted arrays

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""Contains a method that spawns wait_random n times with a
-specified delay between each call."""
+"""
+An async function `wait_n` to run multiple coroutines concurrently and return
+sorted execution delays.
+"""
 import asyncio
 from typing import List
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with a specified delay
-    between each call.
-    Args:
-        n: number of times to spawn wait_random
-        max_delay: maximum delay between each call
-    Returns:
-        list of delays
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    Execute `n` instances of `wait_random` with a maximum delay of `max_delay`
+    and return a list of their delays in ascending order.
+    """
+    list_of_delays = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(list_of_delays)
