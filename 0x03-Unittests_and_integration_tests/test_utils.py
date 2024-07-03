b83813a4a976ff28a43fd572ae2f731701a92726
codenvibes <terrymk1999@gmail.com>
2024-07-03 18:37:05 +0300

    Test: Add unit test for utils.get_json using unittest.mock.patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,9 @@
 Unit tests for the utils.access_nested_map function.
 """
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -36,3 +37,31 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Unit tests for the get_json function
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test case to verify the behavior of get_json function.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call get_json with the test_url
+        result = get_json(test_url)
+
+        # Assertions:
+        # Check if requests.get was called with test_url
+        mock_get.assert_called_once_with(test_url)
+        # Check if get_json returned the expected payload
+        self.assertEqual(result, test_payload)
